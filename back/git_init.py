7e3d6e616d84784aa6ecd7c1ce016959b75c196b
hayeongKo <polarpheno@icloud.com>
2023-05-12 13:57:56 +0900

    [FIX] git add 형식 추가

--- back/git_init.py
+++ back/git_init.py
@@ -5,4 +5,4 @@ def git_init():
         result = subprocess.run(['git', 'init'], capture_output=True, check=True)
         return True, result.stdout.decode()
     except subprocess.CalledProcessError as e:
-        return False, e.stderr.decode()
+        return False, 'Error: '+e.stderr.decode()
