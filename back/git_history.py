52339a1a25fbe2e079d8eeee55ee34c89bcc9e02
hayeongKo <polarpheno@icloud.com>
2023-06-04 16:12:10 +0900

    [FIX] validation 처리

--- back/git_history.py
+++ back/git_history.py
@@ -1,17 +1,22 @@
 import subprocess
-import re
 
 def git_history_list():
     result_lst = []
     command = ['git', 'log', '--graph', '--pretty=format:%C(auto)[%d][%s][%an][%h7]']
     result = subprocess.run(command, capture_output=True, text=True)
-    output = result.stdout.strip()
-    #print(output)
+    
+    ##error
+    if result.returncode == 128:
+        error = result.stderr.strip()
+        result_lst.append(error)
+    else:
+        output = result.stdout.strip()   
+        for line in output.split('\n'):
+            result_lst.append(line)
 
-    for line in output.split('\n'):
-        result_lst.append(line)
-    print(result_lst)
-    return result_lst
+    print(result.returncode, result_lst)
+    return result.returncode, result_lst
 
 def git_history_detail(commit_sum):
-
+    command = ['git', 'show', '--stat', commit_sum]
+    result = subprocess.run(command, capture_output=True, text=True)
