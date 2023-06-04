b46c61b07ba276a38dee003651857a1e15112e71
hayeongKo <polarpheno@icloud.com>
2023-06-04 14:21:32 +0900

    [FEAT] git history

--- back/git_history.py
+++ back/git_history.py
@@ -1,34 +1,48 @@
 import subprocess
 import re
 
-def show_git_commit_graph_with_author():
-    result = subprocess.run(['git', 'log', '--graph', '--pretty=format:%h %p %an <%ae> %s'], capture_output=True, text=True)
+def get_branches_containing_commit(commit_hash):
+    result = subprocess.run(['git', 'branch', '-a', '--contains', commit_hash], capture_output=True, text=True)
     output = result.stdout.strip()
 
-    # 커밋 그래프에서 커밋 정보 추출
+    branches = []
+    # for line in output.split('\n'):
+    #     branch_name = line.strip()
+    #     if branch_name.startswith('*'):
+    #         branch_name = branch_name[1:].strip()
+    #     if branch_name:
+    #         branches.append(branch_name)
+
+    for line in output.split('\n'):
+        branch_name = line.strip()
+        if branch_name.startswith('*'):
+            branch_name = branch_name[1:].strip()
+        if branch_name and not branch_name.startswith('remotes/origin/') and not branch_name.startswith('remotes/'):
+            branches.append(branch_name)
+
+    return branches
+
+def show_git_commit_graph_with_author_and_branch():
+    result_lst = []
+    result = subprocess.run(['git', 'log', '--graph', '--pretty=format:%H %p %an <%ae> %s'], capture_output=True, text=True)
+    output = result.stdout.strip()
+
     commit_lines = re.findall(r'(\s*\*?\s*[\w\d]+\s+[\w\s\d]+\s+[\w\s\d]+)\s+(.*)', output)
     commit_messages = {commit_line[0]: commit_line[1] for commit_line in commit_lines}
 
-    # 그래프 출력
     for line in output.split('\n'):
-        if line.strip() and not line.startswith(' '):
-            print(line)
-        elif line.strip():
+        if line.strip():
             commit_line = line.lstrip()
-            commit_hash = commit_line.split()[0]
+            commit_hash = commit_line.replace('|', ' ').replace('*', ' ').replace('\\', ' ').replace('/', ' ').split()
             commit_info = commit_messages.get(commit_line, '')
-            print(f"{commit_line}  {commit_info}")
+            # print('commit_hash',commit_hash, end='\n')
+            if commit_hash != []:
+                branch_names = get_branches_containing_commit(commit_hash[0])
+                if not branch_names:
+                    branch_names = ['No Branch']
 
-show_git_commit_graph_with_author()
+            result_lst.append(f"{commit_line}  {commit_info}  ({', '.join(branch_names)})")
+    print(result_lst)
+    return result_lst
 
-
-
-
-# import subprocess
-
-# def get_git_commit_logs():
-#     result = subprocess.run(['git', 'log'], capture_output=True, text=True)
-#     output = result.stdout.strip()
-#     print(output)
-
-# get_git_commit_logs()
+# show_git_commit_graph_with_author_and_branch()
