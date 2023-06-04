d8e9968964395947dabd4c262aee4a5f0a005c53
hayeongKo <polarpheno@icloud.com>
2023-06-04 17:20:57 +0900

    [FIX] except 위치 변경

--- back/git_history.py
+++ back/git_history.py
@@ -5,16 +5,18 @@ def git_history_list():
         result_lst = []
         command = ['git', 'log', '--graph', '--pretty=format:%C(auto)[%d][%s][%an][%h7]']
         result = subprocess.run(command, capture_output=True, text=True, check=True)
-        
-        output = result.stdout.strip()
-        for line in output.split('\n'):
-            result_lst.append(line)
-        
-        return 0, result_lst
-    
-    except subprocess.CalledProcessError as e:
+
+    except result.CalledProcessError as e:
         error = e.stderr.strip()
         return e.returncode, [error]
+        
+    output = result.stdout.strip()
+    for line in output.split('\n'):
+        result_lst.append(line)
+    
+    return 0, result_lst
+    
+    
 
 def git_history_detail(checksum):
     try:
@@ -22,12 +24,14 @@ def git_history_detail(checksum):
         command = ['git', 'show', '--stat', checksum]
         result = subprocess.run(command, capture_output=True, text=True, check=True)
         
-        output = result.stdout.strip()
-        for line in output.split('\n'):
-            result_lst.append(line)
-        
-        return 0, result_lst
-    
-    except subprocess.CalledProcessError as e:
+    except result.CalledProcessError as e:
         error = e.stderr.strip()
         return e.returncode, [error]
+        
+    output = result.stdout.strip()
+    for line in output.split('\n'):
+        result_lst.append(line)
+    
+    return 0, result_lst
+    
+    
