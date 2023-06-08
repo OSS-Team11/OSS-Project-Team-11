d84d7da81e49b4acefa790a0a3d6718d96e002a6
hayeongKo <polarpheno@icloud.com>
2023-06-09 01:49:40 +0900

    [FIX] create branch validation 추가

--- back/git_b_create.py
+++ back/git_b_create.py
@@ -2,10 +2,16 @@ import subprocess
 
 def git_b_create(branch_name):
     # git command execution
-    subprocess.run(['git', 'branch', branch_name])
-
-# Input branch name
-branch_name = input("Enter a new branch name: ")
+    try:
+        subprocess.run(['git', 'branch', branch_name])
+        return True, 'Success'
+    except subprocess.CalledProcessError as e:
+        if e.stderr:
+            error_message = e.stderr.strip().decode('utf-8')
+            return False, error_message
+        else:
+            return False, 'Error: failed to commit'
 
 # branch creation function call
-git_b_create(branch_name)
+# git_b_create('neww')
+
