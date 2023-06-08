c23f2e3ba4d485afcb2a2c61d9e7e3019af1308f
hayeongKo <polarpheno@icloud.com>
2023-06-09 01:56:59 +0900

    [FIX] delete branch validation

--- back/git_b_delete.py
+++ back/git_b_delete.py
@@ -2,10 +2,13 @@ import subprocess
 
 def git_b_delete(branch_name):
     # git command execution
-    subprocess.run(['git', 'branch', '-D', branch_name])
-
-# Input: branch name to delete 
-branch_name = input("Enter the branch name to delete : ")
-
-# branch delete function call
-git_b_delete(branch_name)
+    try:
+        subprocess.run(['git', 'branch', '-D', branch_name])
+        return True, 'Success'
+    except subprocess.CalledProcessError as e:
+        if e.stderr:
+            error_message = e.stderr.strip().decode('utf-8')
+            print(error_message)
+            return False, error_message
+        else:
+            return False, 'Error: failed to delete branch'
