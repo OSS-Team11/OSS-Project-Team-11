7ac787d21fbfe029b42165a392481e01bf701a2c
easthee <ark182818@gmail.com>
2023-06-08 15:56:06 +0900

    git_b_create modif

--- back/git_b_create.py
+++ back/git_b_create.py
@@ -1,11 +1,16 @@
 import subprocess
 
 def git_b_create(branch_name):
+    # Change the default branch name from "master" to "main"
+    subprocess.Popen(['git', 'symbolic-ref', 'refs/remotes/origin/HEAD', 'refs/remotes/origin/main'])
+
+
     # git command execution
     subprocess.Popen(['git', 'branch', branch_name])
 
 # Input branch name
 branch_name = input("Enter a new branch name: ")
+
 
 # branch creation function call
 git_b_create(branch_name)
