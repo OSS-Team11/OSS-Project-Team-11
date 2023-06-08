87d21c412f5c18967cacc0f26bbd07c44e449b0f
easthee <ark182818@gmail.com>
2023-06-08 15:54:10 +0900

    add implementation

--- back/git_b_delete.py
+++ back/git_b_delete.py
@@ -1,11 +1,17 @@
 import subprocess
 
 def git_b_delete(branch_name):
-    # git command execution
-    subprocess.run(['git', 'branch', '-D', branch_name])
+    # Check if the branch exists
+    output = subprocess.check_output(['git', 'branch', '-r', '--list', f'origin/{branch_name}']).decode('utf-8')
+    if output.strip():
+        # git command execution
+        subprocess.run(['git', 'branch', '-D', branch_name])
+        print(f"The branch '{branch_name}' has been deleted.")
+    else:
+        print(f"The branch '{branch_name}' does not exist.")
 
-# Input: branch name to delete 
-branch_name = input("Enter the branch name to delete : ")
+# Input: branch name to delete
+branch_name = input("Enter the branch name to delete: ")
 
 # branch delete function call
 git_b_delete(branch_name)
