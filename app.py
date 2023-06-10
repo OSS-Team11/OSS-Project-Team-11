80344d8bd7e1cadc8c0225941391992711569dc4
devJS00 <jisuejs@gmail.com>
2023-06-10 22:52:58 +0900

    [FEAT] merge fuction complete

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ from back.git_b_delete import *
 from back.git_b_rename import *
 from back.git_b_checkout import *
 from back.git_clone import *
+from back.git_merge import *
 
 # Interface
 
@@ -1050,6 +1051,56 @@ delete_bttn.pack(side="left", expand=1)
 rename_bttn.pack(side="left", expand=1)
 checkout_bttn.pack(side="left", expand=1)
 
+########## merge 영역 ##############
+def merge_bttn_clicked():
+    selected_brnch = select_branch("<ButtonRelease-1>")
+    success, message = git_merge(selected_brnch)
+    co_new_win.destroy()
+    show_message(message)
+
+def merge_new_window():
+    success, message = get_current_branch()
+    if success == True:
+        global mrg_new_win
+        mrg_new_win = Toplevel()
+        mrg_new_win.title("Merge")
+        mrg_new_win.attributes("-topmost", True)
+
+        frame_branch_list = Frame(mrg_new_win, border=2, relief="groove", bg="white")
+        frame_branch_list.pack(side="top", fill="both", expand=True)
+        global treeview
+        treeview = ttk.Treeview(frame_branch_list, selectmode="extended", show="tree headings", style="mystyle.Treeview")
+        treeview.pack(side="left", expand=1, fill="both")
+        treeview.heading("#0", text="branch list")
+        style = ttk.Style()
+        style.configure("Treeview", rowheight=30, font=("Arial", 12))
+        style.configure("Treeview.Heading", font=("Arial", 12), foreground="grey")
+        style.layout("mystyle.Treeview", [("mystyle.Treeview.treearea", {"sticky":"nswe"})])
+        scrollbar = ttk.Scrollbar(frame_branch_list, orient="vertical", command=treeview.yview)
+        treeview.configure(yscroll=scrollbar.set)
+        scrollbar.pack(side="right",fill="y")
+        
+        cnfrm_button = Button(mrg_new_win, text="merge", relief="flat", bg="white", command=merge_bttn_clicked)
+        cnfrm_button.pack(side="bottom")
+
+        treeview.delete(*treeview.get_children())
+        success, curr_branch = get_current_branch()
+        branch_list = get_branches()
+        for i in range(len(branch_list[1])):
+            if branch_list[1][i] != curr_branch: # 현재 branch는 제외
+                treeview.insert("", tk.END, text=branch_list[1][i], values= "", open=False, image=branch_icon)
+
+        treeview.bind('<ButtonRelease-1>', select_branch)
+
+    elif success == False:
+        show_message(message)
+
+
+merge_bttn = tk.Button(frame_branch_command, text="merge", font=("Arial", 12), relief="flat", bg="white", fg="black", width = 15, command=merge_new_window)
+merge_bttn.pack(side="left", expand=1)
+
+
+
 ############# commit history 영역 ############
 def _on_mousewheel(event):
     canvas.yview_scroll(-1 * (event.delta // 120), "units")
@@ -1140,8 +1191,7 @@ def clone_bttn_clicked(rad_var, addr_entry, id_entry, token_entry):
     addr=addr_entry.get() # 입력받은 commit message commit_message에 저장
     id=id_entry.get()
     token=token_entry.get()
-    print(rad_selected)
-    
+
     success, message = git_clone(rad_selected, addr, id, token)
     if success:
         update_files(entry.get())
