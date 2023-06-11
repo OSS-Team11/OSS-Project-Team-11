760d34e48b3fcb7994fa2a5350f464472de39a7e
hayeongKo <polarpheno@icloud.com>
2023-06-11 21:29:47 +0900

    [FIX] return 형식 찐막

--- back/git_history.py
+++ back/git_history.py
@@ -3,7 +3,7 @@ import subprocess
 def git_history_list():
     try:
         result_lst = []
-        command = ['git', 'log', '--graph', '--pretty=format:{%d}{%s}{%an}{%h}']
+        command = ['git', 'log', '--graph', '--pretty=format:{%d{%s      %an{%h']
         result = subprocess.run(command, capture_output=True, text=True, check=True)
 
         if result.stdout is not None:
@@ -11,15 +11,22 @@ def git_history_list():
             for line in output.split('\n'):
                 history_detail = []
                 history = line.replace('{', '^').replace('}', '^').split('^')
-                cnt = 0
-                for word in history:
-                    cnt += 1
-                    if cnt == 2 and word == '':
-                        history_detail.append(word)
-                    if word != '':
-                        history_detail.append(word)
-                result_lst.append(history_detail)
-        # print(result_lst)
+                # print(history)
+                buffer = ''
+                for i in range(len(history)):
+                    if i == 0 or i == 3:
+                        history_detail.append(history[i])
+                    elif i == 1:
+                        if history[1] != '':
+                            buffer += history[1]
+                            buffer += '      '
+                    elif i == 2:
+                        buffer += history[2]
+                        history_detail.append(history[i])  
+                        print(buffer)
+                result_lst.append(history_detail)  
+
+        #print(result_lst)
         return 0, result_lst
 
     except subprocess.CalledProcessError as e:
@@ -57,5 +64,5 @@ def git_history_detail(checksum):
         return e.returncode, [error]
 
 
-#git_history_list()    
+git_history_list()    
 #git_history_detail('62d881117692eaa1c9f10003eb3aec7497e0f8c5')
